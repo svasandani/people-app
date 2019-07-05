114d2d57a45ace99cc55c360a15afcc54b71617c
Shailesh Vasandani <vasandanishailesh@gmail.com>
2019-07-05 15:33:35 +0800

    implemented angular material and angular flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { ArtistItemComponent } from './artist-item/artist-item.component';
@@ -16,7 +19,7 @@ import { SearchPipe } from './pipe-search/pipe.search';
     SearchPipe
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, MaterialModule, BrowserAnimationsModule, FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatToolbarModule,
+  MatCardModule,
+  MatInputModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule
+  ]
+})
+
+export class MaterialModule { }
